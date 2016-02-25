9d907cc76607996091b89b42bc73b193a59e4dbd
lolito <lolito@projectcodex.co>
2016-02-25 08:38:08 +0200

    Count Down Java program
    This program accepts a number and start counting down from it.

---
+++ contributors/add-Likhona7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+java class add-Likhona7 {
+  public static void main (String args[]){
+    // Make an object input
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter the number to count down from : ");
+    int number = input.nextInt();
+    
+    for (count=1; count <10; count++){
+      System.out.println("Count = " + count);
+    }
+  }
+}
